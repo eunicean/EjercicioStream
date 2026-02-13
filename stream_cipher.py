f45f15e049930f191cee699fd45f5c6ddcdf47db
eunicean <eunice.mata2992@gmail.com>
2026-02-12 18:19:27 -0600

    función de cifrado

--- stream_cipher.py
+++ stream_cipher.py
@@ -1,10 +1,26 @@
 import random
 
-def generateKeystream(size: int, seed: int):
-    random.seed(seed)
+def generateKeystream(size: int, clave: int):
+    random.seed(clave)
     key = []
-    
+
     for i in range(size):
         key.append(random.randint(0, 255))
     
     return key
+
+def cifrar(mensaje: str, clave: int) -> bytes:
+    print(f"Mensaje recibido: {mensaje}")
+    mensaje_bytes = mensaje.encode('utf-8')
+    print(f"Mensaje en bytes: {mensaje_bytes}")
+
+    keystream = generateKeystream(len(mensaje_bytes), clave)
+    print(f"Keystream: {keystream}")
+
+    mensaje_cifrado = bytes([mb ^ ks for mb, ks in zip(mensaje_bytes, keystream)])
+    print(f"Mensaje cifrado: {mensaje_cifrado}")
+
+    return mensaje_cifrado
+
+texto = "Tarea cargada"
+cifrar(texto, 15)
