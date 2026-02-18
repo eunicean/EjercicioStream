3fa6a15e0ebabd3eb99b41d4c2a6e82c5f8b1ef5
eunicean <eunice.mata2992@gmail.com>
2026-02-17 23:50:38 -0600

    commented prints  for part3

--- stream_cipher.py
+++ stream_cipher.py
@@ -14,24 +14,24 @@ def generateKeystream(size: int, clave: int):
 # https://chatgpt.com/share/698e729c-aba0-800f-8104-f5303d1999fb
 # después le pregunté si el proceso de cifrar es el mismo para descifrar
 def cifrar(mensaje: str, clave: int) -> bytes:
-    print(f"Mensaje recibido: {mensaje}")
+    # print(f"Mensaje recibido: {mensaje}")
     mensaje_bytes = mensaje.encode('utf-8')
-    print(f"Mensaje en bytes: {mensaje_bytes}")
+    # print(f"Mensaje en bytes: {mensaje_bytes}")
 
     keystream = generateKeystream(len(mensaje_bytes), clave)
-    print(f"Keystream: {keystream}")
+    # print(f"Keystream: {keystream}")
 
     mensaje_cifrado = bytes([mb ^ ks for mb, ks in zip(mensaje_bytes, keystream)])
-    print(f"Mensaje cifrado: {mensaje_cifrado}")
+    # print(f"Mensaje cifrado: {mensaje_cifrado}")
 
     return mensaje_cifrado
 
 def decifrar(mensaje_cifrado: bytes, clave: int) -> str:
     keystream = generateKeystream(len(mensaje_cifrado), clave)
-    print(f"Keystream para desencriptar: {keystream}")
+    # print(f"Keystream para desencriptar: {keystream}")
 
     mensaje_descifrado = bytes([mb ^ ks for mb, ks in zip(mensaje_cifrado, keystream)])
-    print(f"Mensaje desencriptado: {mensaje_descifrado}")
+    # print(f"Mensaje desencriptado: {mensaje_descifrado}")
 
     return mensaje_descifrado
 
